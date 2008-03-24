958716809ff64391ec1add95bd9a14edafe97b27
David Sveningsson <ext@sidvind.com>
2008-03-24 01:41:03 +0000

    motd
    svn path=/trunk/; revision=54

--- frontend/pages/main.php
+++ frontend/pages/main.php
@@ -7,9 +7,23 @@ class Main extends Module {
   function index(){
     Module::set_template('main.tmpl');
 
-    return array(
-		  'slides' => get_slides()
-		  );
+    $ret = array();
+
+    $ret['slides'] = get_slides();
+
+    global $BasePath;
+    $motd_file = "$BasePath/motd";
+
+    if ( file_exists($motd_file) ){
+      $file = fopen("$BasePath/motd", "r");
+      $content = str_replace("\n", "<br/>\n", fread($file, filesize($motd_file)));
+
+      if ( strlen($content) > 0 ){
+	$ret['motd'] = $content;
+      }
+    } 
+
+    return $ret;
   }
 };
 
