968b6f090e88424df02104a6604ae25f7a4441ab
David Sveningsson <ext@sidvind.com>
2008-06-16 22:08:00 +0000

    Updated the installation sql
    svn path=/branches/slide_bins/; revision=121

--- frontend/maintenance/install.sql
+++ frontend/maintenance/install.sql
@@ -1,12 +1,22 @@
+--
+-- Table structure for table `bins`
+--
 
+CREATE TABLE `bins` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` text,
+  UNIQUE KEY `id` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
 CREATE TABLE `files` (
   `id` int(11) NOT NULL auto_increment,
   `fullpath` text,
+  `bin_id` int(11) NOT NULL default '0',
+  `active` tinyint(1) NOT NULL default '1',
   PRIMARY KEY  (`id`)
-);
-
-CREATE TABLE `settings` (
-  `id` int(11) NOT NULL auto_increment,
-  PRIMARY KEY  (`id`)
-);
-
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
