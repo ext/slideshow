1e12ef8acd445e6750e92149d9b526d8c3b260b2
David Sveningsson <ext@sidvind.com>
2008-09-03 13:29:13 +0000

    Missing file
    svn path=/branches/ext/; revision=163

---
+++ frontend/core/page_exception.php
@@ -0,0 +1,28 @@
+<?
+/**
+ * This file is part of Slideshow.
+ * Copyright (C) 2008 David Sveningsson <ext@sidvind.com>
+ *
+ * Slideshow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Slideshow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Slideshow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+class PageException extends Exception {
+
+}
+
+define('UPLOAD_ERROR', 1);
+define('EXECUTABLE_ERROR', 2);
+define('FILE_NOT_FOUND', 404);
+
+?>
