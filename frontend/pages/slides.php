073c603501160fa2e9c98e43162fe23953d7fce9
David Sveningsson <ext@sidvind.com>
2008-09-03 11:42:57 +0000

    Handles to large uploaded files
    svn path=/branches/ext/; revision=160

--- frontend/pages/slides.php
+++ frontend/pages/slides.php
@@ -76,7 +76,30 @@ class Slides extends Module {
 		);
 	}
 
-  function submit_image(){
+	function submit_image(){
+		$upload_ok = $_FILES['filename']['error'] === UPLOAD_ERR_OK;
+
+		if ( !$upload_ok ){
+
+			$error_code = $_FILES['filename']['error'];
+
+			$error_string = array(
+			    UPLOAD_ERR_INI_SIZE => 'The uploaded file exceeds the upload_max_filesize directive in php.ini.',
+			    UPLOAD_ERR_FORM_SIZE => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.',
+			    UPLOAD_ERR_PARTIAL => 'The uploaded file was only partially uploaded.',
+			    UPLOAD_ERR_NO_FILE => 'No file was uploaded.',
+			    UPLOAD_ERR_NO_TMP_DIR => 'Missing a temporary folder.',
+			    UPLOAD_ERR_CANT_WRITE => 'Failed to write file to disk.',
+			    UPLOAD_ERR_EXTENSION => 'File upload stopped by extension.',
+			);
+
+			if ( isset($error_string[$error_code]) ){
+		        throw new Exception($error_string[$error_code]);
+			} else {
+		        throw new Exception("Unknown error uploading file.");
+			}
+		}
+
 	global $settings;
 
 	$convert = $settings->convert_binary();
