aafc5e3321cd6796f0480f6dc4d479a21aedbfae
David Sveningsson <ext@sidvind.com>
2008-09-12 16:11:27 +0000

    Renamed collection to queue
    svn path=/trunk/; revision=208

--- frontend/templates/main.php
+++ frontend/templates/main.php
@@ -31,7 +31,7 @@
 				<li class="first<? if ( $path->module() == 'main' ){ ?> bajs<? } ?>"><a class="menu_main" href="/index.php/">Main</a></li>
 				<li<? if ( $path->module() == 'slides' ){ ?> class="bajs"<? } ?> ><a class="menu_slide" href="/index.php/slides/upload">Slides</a></li>
 				<li<? if ( $path->module() == 'video' ){ ?> class="bajs"<? } ?>><a class="menu_video" href="/index.php/video">Video</a></li>
-				<li<? if ( $path->module() == 'bins' ){ ?> class="bajs"<? } ?>><a class="menu_bins" href="/index.php/bins">Collections</a></li>
+				<li<? if ( $path->module() == 'bins' ){ ?> class="bajs"<? } ?>><a class="menu_bins" href="/index.php/bins">Queues</a></li>
 				<li class="last<? if ( $path->module() == 'maintenance' ){ ?> bajs<? } ?>"><a class="menu_maintenance" href="/index.php/maintenance">Maintenance</a></li>
 			</ul>
 		</div>
