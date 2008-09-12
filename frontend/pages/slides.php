eaf2122184c0de20ebccf864c3d9ecbfb773f255
David Sveningsson <ext@sidvind.com>
2008-09-12 15:08:42 +0000

    Correct indention
    svn path=/trunk/; revision=207

--- frontend/pages/slides.php
+++ frontend/pages/slides.php
@@ -109,111 +109,110 @@ class Slides extends Module {
 			}
 		}
 
-	global $settings;
+		global $settings;
 
-	$convert = $settings->convert_binary();
+		$convert = $settings->convert_binary();
 
-	if ( !file_exists($convert) ){
-	  throw new Exception("Could not find ImageMagick 'convert' executable.");
+		if ( !file_exists($convert) ){
+			throw new Exception("Could not find ImageMagick 'convert' executable.");
+		}
+
+		$name = $_FILES['filename']['name'];
+		$hash = md5(uniqid());
+		$fullpath = "{$settings->image_path()}/{$hash}_$name";
+
+		$uploaded = $_FILES['filename']['tmp_name'];
+
+		if ( !is_uploaded_file( $uploaded ) ){
+			die("Handling file that was not uploaded");
+		}
+
+		$resolution = $settings->resolution_as_string();
+
+		move_uploaded_file($uploaded, $fullpath);
+
+		$this->_convert($fullpath, $fullpath, $resolution);
+		$this->_convert($fullpath, $fullpath . '.thumb.jpg', "200x200");
+
+		q("INSERT INTO files (fullpath, type, title) VALUES ('$fullpath', 'image', '" . mysql_real_escape_string($name) . "')");
+
+		global $daemon;
+		$daemon->reload_queue();
+
+		Module::log("Uploaded $name");
+		Module::redirect('/index.php');
 	}
 
-	$name = $_FILES['filename']['name'];
-	$hash = md5(uniqid());
-	$fullpath = "{$settings->image_path()}/{$hash}_$name";
-
-	$uploaded = $_FILES['filename']['tmp_name'];
-
-	if ( !is_uploaded_file( $uploaded ) ){
-	  die("Handling file that was not uploaded");
+	function _utf_hack($str){
+		return strtr($str, array(
+			"Å" => "&#0197;",
+			"Ä" => "&#0196;",
+			"Ö" => "&#0214;",
+			"å" => "&#0229;",
+			"ä" => "&#0228;",
+			"ö" => "&#0246;"
+		));
 	}
 
-	$resolution = $settings->resolution_as_string();
+	function preview(){
+		$title = $this->_utf_hack(html_entity_decode(get('title'), ENT_NOQUOTES, 'UTF-8'));
+		$content = explode("\n", $this->_utf_hack(html_entity_decode(get('content'), ENT_NOQUOTES, 'UTF-8')) );
 
-	move_uploaded_file($uploaded, $fullpath);
+		$align = $_GET['align'];
 
-	$this->_convert($fullpath, $fullpath, $resolution);
-	$this->_convert($fullpath, $fullpath . '.thumb.jpg', "200x200");
+		$this->_create_image($title, $content, $align);
 
-	q("INSERT INTO files (fullpath, type, title) VALUES ('$fullpath', 'image', '" . mysql_real_escape_string($name) . "')");
-
-	global $daemon;
-	$daemon->reload_queue();
-
-	Module::log("Uploaded $name");
-	Module::redirect('/index.php');
-  }
-
-  function _utf_hack($str){
-	return strtr($str,
-		 array(
-			   "Å" => "&#0197;",
-			   "Ä" => "&#0196;",
-			   "Ö" => "&#0214;",
-			   "å" => "&#0229;",
-			   "ä" => "&#0228;",
-			   "ö" => "&#0246;"
-			   ));
-  }
-
-  function preview(){
-	$title = $this->_utf_hack(html_entity_decode(get('title'), ENT_NOQUOTES, 'UTF-8'));
-	$content = explode("\n", $this->_utf_hack(html_entity_decode(get('content'), ENT_NOQUOTES, 'UTF-8')) );
-
-	$align = $_GET['align'];
-
-	$this->_create_image($title, $content, $align);
-
-	exit();
-  }
-
-  function _create_image($title, $content, $alignment, $filename = NULL){
-	global $settings;
-
-	$resolution = $settings->resolution();
-
-	$bgfile = $settings->background();
-
-	$im = false;
-	if ( !empty($bgfile) && is_readable($bgfile)  ){
-		///@todo This only accepts PNG's
-		$im = @imagecreatefrompng($bgfile);
+		exit();
 	}
 
-	if ( !$im ){
-	  $im  = imagecreatetruecolor($resolution[0], $resolution[1]);
-	  $black  = imagecolorallocate($im, 0, 0, 0);
-	  $white  = imagecolorallocate($im, 255, 255, 255);
-	  imagefilledrectangle($im, 0, 0, $resolution[0], $resolution[1], $black);
-	} else {
-	  $black  = imagecolorallocate($im, 0, 0, 0);
-	  $white  = imagecolorallocate($im, 255, 255, 255);
+	function _create_image($title, $content, $alignment, $filename = NULL){
+		global $settings;
+
+		$resolution = $settings->resolution();
+
+		$bgfile = $settings->background();
+
+		$im = false;
+		if ( !empty($bgfile) && is_readable($bgfile)  ){
+			///@todo This only accepts PNG's
+			$im = @imagecreatefrompng($bgfile);
+		}
+
+		if ( !$im ){
+			$im  = imagecreatetruecolor($resolution[0], $resolution[1]);
+			$black  = imagecolorallocate($im, 0, 0, 0);
+			$white  = imagecolorallocate($im, 255, 255, 255);
+			imagefilledrectangle($im, 0, 0, $resolution[0], $resolution[1], $black);
+		} else {
+			$black  = imagecolorallocate($im, 0, 0, 0);
+			$white  = imagecolorallocate($im, 255, 255, 255);
+		}
+
+		$font = $settings->font();
+
+		if ( !(empty($font) && is_readable($font) ) ){
+			$font = "../Vera.ttf";
+		}
+
+		///@note Magic numbers
+		$title_size = 28;
+		$content_size = 12;
+
+		// 1 is alignment (center)
+		// 100 is y-coordinate
+		$this->_render_string_aligned($im, 1, 100, $title_size, $font, $white, $title);
+
+		$y = 180;
+		foreach ( $content as $paragraph ){
+			$y = $this->_render_string_aligned($im, $alignment, $y, $content_size, $font, $white, $paragraph);
+		}
+
+		if ( $filename == NULL ){
+			header("Content-Type: image/png");
+		}
+
+		imagepng($im, $filename);
 	}
-
-	$font = $settings->font();
-
-	if ( !(empty($font) && is_readable($font) ) ){
-		$font = "../Vera.ttf";
-	}
-
-	///@note Magic numbers
-	$title_size = 28;
-	$content_size = 12;
-
-	// 1 is alignment (center)
-	// 100 is y-coordinate
-	$this->_render_string_aligned($im, 1, 100, $title_size, $font, $white, $title);
-
-	$y = 180;
-	foreach ( $content as $paragraph ){
-	  $y = $this->_render_string_aligned($im, $alignment, $y, $content_size, $font, $white, $paragraph);
-	}
-
-	if ( $filename == NULL ){
-	  header("Content-Type: image/png");
-	}
-
-	imagepng($im, $filename);
-  }
 
 	function _convert($src, $dst, $resolution){
 		global $settings;
@@ -230,66 +229,65 @@ class Slides extends Module {
 		}
 	}
 
-  function _render_string_aligned($im, $alignment, $y, $size, $font, $color, $string){
-	global $settings;
+	function _render_string_aligned($im, $alignment, $y, $size, $font, $color, $string){
+		global $settings;
 
-	$resolution = $settings->resolution();
-	$width = $resolution[0];
-	$margin = 60;
+		$resolution = $settings->resolution();
+		$width = $resolution[0];
+		$margin = 60;
 
-	$line_spacing = (int)($size * 1.5);
+		$line_spacing = (int)($size * 1.5);
 
-	$words = explode(" ", $string);
-	$part = array_shift($words);
+		$words = explode(" ", $string);
+		$part = array_shift($words);
 
-	$data = imagettfbbox( $size, 0, $font, $part );
-	$old_part_width = $data[2] - $data[0];
+		$data = imagettfbbox( $size, 0, $font, $part );
+		$old_part_width = $data[2] - $data[0];
 
-	foreach ( $words as $word ){
-	  $data = imagettfbbox( $size, 0, $font, "$part $word" );
-	  $part_width = $data[2] - $data[0];
+		foreach ( $words as $word ){
+			$data = imagettfbbox( $size, 0, $font, "$part $word" );
+			$part_width = $data[2] - $data[0];
 
-	  if ( $part_width > $width - $margin * 2 ){
-	switch ( $alignment ){
-	case 0:	$this->_render_string_left( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
-	case 1:	$this->_render_string_center( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
-	case 2:	$this->_render_string_right( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
-	default: die("This alignemnt ($alignment) is not implemented yet");
+			if ( $part_width > $width - $margin * 2 ){
+				switch ( $alignment ){
+					case 0:	$this->_render_string_left( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
+					case 1:	$this->_render_string_center( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
+					case 2:	$this->_render_string_right( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
+					default: die("This alignemnt ($alignment) is not implemented yet");
+				}
+
+				$part = $word;
+				$data = imagettfbbox( $size, 0, $font, $word );
+				$old_part_width = $data[2] - $data[0];
+				$y += $line_spacing;
+				continue;
+			}
+
+			$part .= " $word";
+			$old_part_width = $part_width;
+		}
+
+		switch ( $alignment ){
+			case 0:	$this->_render_string_left( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
+			case 1:	$this->_render_string_center( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
+			case 2:	$this->_render_string_right( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
+			default: die("This alignemnt ($alignment) is not implemented yet");
+		}
+
+		return $y + $line_spacing;
 	}
 
-	$part = $word;
-	$data = imagettfbbox( $size, 0, $font, $word );
-	$old_part_width = $data[2] - $data[0];
-	$y += $line_spacing;
-	continue;
-	  }
-
-	  $part .= " $word";
-	  $old_part_width = $part_width;
+	function _render_string_left($im, $x, $y, $margin, $width, $size, $font, $color, $string){
+		imagefttext( $im, $size, 0, $margin, $y, $color, $font, $string );
 	}
 
-	switch ( $alignment ){
-	case 0:	$this->_render_string_left( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
-	case 1:	$this->_render_string_center( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
-	case 2:	$this->_render_string_right( $im, $old_part_width, $y, $margin, $width, $size, $font, $color, $part ); break;
-	default: die("This alignemnt ($alignment) is not implemented yet");
+	function _render_string_center($im, $x, $y, $margin, $width, $size, $font, $color, $string){
+		imagefttext( $im, $size, 0, $width/2-$x/2, $y, $color, $font, $string );
 	}
 
-	return $y + $line_spacing;
-  }
-
-
-  function _render_string_left($im, $x, $y, $margin, $width, $size, $font, $color, $string){
-	imagefttext( $im, $size, 0, $margin, $y, $color, $font, $string );
-  }
-
-  function _render_string_center($im, $x, $y, $margin, $width, $size, $font, $color, $string){
-	imagefttext( $im, $size, 0, $width/2-$x/2, $y, $color, $font, $string );
-  }
-
-  function _render_string_right($im, $x, $y, $margin, $width, $size, $font, $color, $string){
-	imagefttext( $im, $size, 0, $width - $margin - $x, $y, $color, $font, $string );
-  }
+	function _render_string_right($im, $x, $y, $margin, $width, $size, $font, $color, $string){
+		imagefttext( $im, $size, 0, $width - $margin - $x, $y, $color, $font, $string );
+	}
 
 	function delete(){
 		global $daemon;
