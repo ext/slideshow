3e0e71b9e3497aabde11a79fe7cd9c4b854ecdec
David Sveningsson <ext@sidvind.com>
2009-07-09 14:27:01 +0000

    missing dir
    svn path=/trunk/; revision=503

---
+++ src/assembler/image.c
@@ -0,0 +1,32 @@
+/**
+ * This file is part of Slideshow.
+ * Copyright (C) 2008 David Sveningsson <ext@sidvind.com>
+ *
+ * Slideshow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Slideshow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Slideshow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "module.h"
+#include "slidelib.h"
+
+MODULE_INFO("image", ASSEMBLER_MODULE, "David Sveningsson");
+
+int module_init(){
+}
+
+int module_cleanup(){
+}
+
+void assemble(const slide_t* slide, const resolution_t* resolution){
+
+}
