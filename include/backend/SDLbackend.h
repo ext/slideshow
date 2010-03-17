d1fab48bb68c9a5d811dcd7abfddef6fe216baa9
David Sveningsson <ext@sidvind.com>
2010-03-17 12:19:48 +0000

    adding sdl backend
    svn path=/trunk/; revision=555

---
+++ include/backend/SDLbackend.h
@@ -0,0 +1,28 @@
+#ifndef SDLBACKEND_H
+#define SDLBACKEND_H
+
+#include "backend/platform.h"
+#include <SDL/SDL.h>
+
+namespace blueflower {
+
+	class SDLBackend:public PlatformBackend {
+		public:
+			SDLBackend();
+			virtual ~SDLBackend();
+
+			virtual int init(const Vector2ui &resolution);
+			virtual void cleanup();
+
+			virtual void poll();
+
+			virtual void swap_buffers() const;
+
+			virtual void lock_mouse(bool state);
+
+		private:
+			bool _lock;
+	};
+}
+
+#endif /* SDLBACKEND_H */
---
+++ src/backend/SDLbackend.cpp
@@ -0,0 +1,59 @@
+#include "backend/SDLbackend.h"
+#include "exception.h"
+
+SDLBackend::SDLBackend()
+	: PlatformBackend()
+	, _lock(false) {
+
+}
+
+SDLBackend::~SDLBackend(){
+
+}
+
+int SDLBackend::init(const Vector2ui &resolution){
+	set_resolution(resolution.width, resolution.height);
+
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_SetVideoMode(resolution.width, resolution.height, 0, SDL_OPENGL);
+	SDL_EnableUNICODE(1);
+
+#ifdef WIN32
+	SetConsoleOutputCP(65001);
+#endif
+
+	return 0;
+}
+
+void SDLBackend::cleanup(){
+	SDL_Quit();
+}
+
+void SDLBackend::poll(){
+	SDL_Event event;
+		while(SDL_PollEvent(&event) ){
+			switch(event.type){
+				case SDL_VIDEORESIZE:
+					printf("video resize\n");
+					set_resolution(event.resize.w, event.resize.h);
+					app->resize(event.resize.w, event.resize.h);
+					break;
+
+				case SDL_QUIT:
+					app->quit();
+					break;
+			}
+		}
+}
+
+void SDLBackend::swap_buffers() const {
+	SDL_GL_SwapBuffers();
+}
+
+void SDLBackend::lock_mouse(bool state){
+	_lock = state;
+
+	if ( _lock ){
+		SDL_WarpMouse(center().x, center().y);
+	}
+}
