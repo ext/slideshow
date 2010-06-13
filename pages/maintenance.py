64d360f5e71bb609e0b148acea9fb97c912662af
David Sveningsson <ext@sidvind.com>
2010-06-13 11:05:07 +0000

    svn path=/branches/slideshow-python/; revision=661

--- pages/maintenance.py
+++ pages/maintenance.py
@@ -3,9 +3,17 @@
 
 import cherrypy
 from lib import queue, slide, template
+from settings import Settings
 
 class Handler(object):
 	@cherrypy.expose
 	@template.output('maintenance/index.html')
 	def index(self):
 		return template.render()
+
+	@cherrypy.expose
+	@template.output('maintenance/config.html')
+	def config(self, action=None):
+		settings = Settings('settings.xml', 'settings.json')
+		
+		return template.render(settings=settings)
--- src/xquery.c
+++ src/xquery.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <assert.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/Xrandr.h>
+#include <Python.h>
+
+static Display* dpy = NULL;
 
 static float mode_refresh (XRRModeInfo *mode_info){
 	if (mode_info->hTotal && mode_info->vTotal){
@@ -11,25 +10,6 @@ static float mode_refresh (XRRModeInfo *mode_info){
 	} else {
 		return 0;
 	}
-}
-
-static void resolutions_for_screen(Display* dpy, int screen_num){
-	Window root = XRootWindow(dpy, screen_num);
-
-	int minWidth;
-	int minHeight;
-	int maxWidth;
-	int maxHeight;
-
-	XRRGetScreenSizeRange(dpy, root, &minWidth, &minHeight, &maxWidth, &maxHeight);
-	XRRScreenResources* res = XRRGetScreenResources (dpy, root);
-	assert(res);
-
-	for ( int o = 0; o < res->nmode; o++ ){
-		printf("\t%d %ux%u@%.1f\n", o, res->modes[o].width, res->modes[o].height, mode_refresh(&res->modes[o]));
-	}
-
-	XRRFreeScreenResources(res);
 }
 
 /**
@@ -40,12 +20,13 @@ static void resolutions_for_screen(Display* dpy, int screen_num){
  *
  * @param display_string
  */
-int parse_screen(const char* display_string){
+static int parse_screen(const char* display_string){
 	int screen = 0;
 
-	if ( display_string[0] == ':' ){
-		int dummy;
-		sscanf(display_string, ":%d.%d", &dummy, &screen);
+	if ( !display_string ){
+		return DefaultScreen(dpy);
+	} else if ( display_string[0] == ':' ){
+		sscanf(display_string, ":%*d.%d", &screen);
 	} else {
 		sscanf(display_string, "%d", &screen);
 	}
@@ -53,43 +34,80 @@ int parse_screen(const char* display_string){
 	return screen;
 }
 
-int main(int argc, const char* argv[]){
-	Display* dpy = XOpenDisplay(NULL);
-	assert(dpy);
+static PyObject* xquery_screens(PyObject *self, PyObject *args) {
+	PyObject* l = NULL;
+	int n = 0, i = 0;
+	char buf[16];
 
-	if ( argc == 1 ){
-		int screen_count = XScreenCount(dpy);
-		printf("%d available screens\n", screen_count);
-
-		for ( int screen_num = 0; screen_num < screen_count; screen_num++ ){
-			printf("screen :0.%d\n", screen_num);
-			resolutions_for_screen(dpy, screen_num);
-		}
-
-		return 0;
+	if ( !dpy ){
+		/* @todo set exception object */
+		return NULL;
 	}
 
-	for ( int i = 1; i < argc; i++ ){
-		if ( strcmp(argv[i], "--screen") == 0 ){
-			if ( ++i<argc ){
-				int screen = parse_screen(argv[i]);
-				resolutions_for_screen(dpy, screen);
-			} else {
-				fprintf(stderr, "--screen takes one argument\n");
-			}
-			continue;
-		}
+	n = XScreenCount(dpy);
+	l = PyList_New(n);
 
-		if ( strcmp(argv[i], "--get-screens") == 0 ){
-			int screen_count = XScreenCount(dpy);
-			for ( int screen_num = 0; screen_num < screen_count; screen_num++ ){
-				printf(":0.%d\n", screen_num);
-			}
-			continue;
-		}
+	for ( i = 0; i < n; i++ ){
+		snprintf(buf, 16, ":0.%d", i);
+		PyList_SET_ITEM(l, i, Py_BuildValue("s", buf));
 	}
 
-	XCloseDisplay(dpy);
+	return l;
+}
 
-	return 0;
+static PyObject* xquery_resolution(PyObject *self, PyObject *args) {
+	char* string = NULL;
+	int screen, o;
+	Window root;
+	XRRScreenResources* res;
+	PyObject* l;
+
+	int min_width;
+	int min_height;
+	int max_width;
+	int max_height;
+
+	if ( !PyArg_ParseTuple(args, "|s", &string) ){
+		return NULL;
+	}
+
+	if ( !dpy ){
+		/* @todo set exception object */
+		return NULL;
+	}
+
+	screen = parse_screen(string);
+	root = XRootWindow(dpy, screen);
+
+	XRRGetScreenSizeRange(dpy, root, &min_width, &min_height, &max_width, &max_height);
+	res = XRRGetScreenResources (dpy, root);
+
+	l = PyList_New(res->nmode);
+
+	for ( o = 0; o < res->nmode; o++ ){
+		PyList_SET_ITEM(l, o, Py_BuildValue("(iif)", res->modes[o].width, res->modes[o].height, mode_refresh(&res->modes[o])));
+	}
+
+	XRRFreeScreenResources(res);
+
+	return l;
+}
+
+//Method table
+static PyMethodDef XQueryMethods[] = {
+	{"screens", xquery_screens, METH_VARARGS, "List all screens"},
+	{"resolutions", xquery_resolution, METH_VARARGS, "List all resolutions for a screen. Defaults to current screen"},
+	{NULL, NULL, 0, NULL}
+};
+
+
+PyMODINIT_FUNC initxquery(void) {
+	PyObject* m;
+
+	m = Py_InitModule("xquery", XQueryMethods);
+	if ( !m ){
+		return;
+	}
+
+	//dpy = XOpenDisplay(NULL);
 }
