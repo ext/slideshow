eec2e06abd7d478f8f693dcb747e5f72f94a4447
David Sveningsson <ext@sidvind.com>
2010-06-22 20:49:30 +0000

    text assembler
    svn path=/branches/slideshow-python/; revision=712

--- lib/assembler/__init__.py
+++ lib/assembler/__init__.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
-import array, cairo, json
+import array, cairo, pango, pangocairo, json, re
+import xml
 from xml.dom import minidom
 
 class Assembler:
@@ -54,45 +55,149 @@ class ImageAssembler(Assembler):
 		else:
 			return (1024, 768)
 
+def decode_color(str):
+	if str[0] == '#':
+		str = str[1:]
+	
+	n = len(str)
+	fmt = {
+		3: '([0-9A-Fa-f]{1})' * 3, # shorthand RGB
+		4: '([0-9A-Fa-f]{1})' * 4, # shorthand RGBA
+		6: '([0-9A-Fa-f]{2})' * 3, # RGB
+		8: '([0-9A-Fa-f]{2})' * 3 # RGBA
+	}
+	
+	match = re.match(fmt[n], str)
+	
+	if match:
+		color = match.groups()
+		if len(color) == 3:
+			color += ('ff',) # force RGBA
+		
+		color = tuple([int(x,16)/255.0 for x in color])
+		
+		return color
+	else:
+		return None
+
+def decode_position(str, size):
+	"""
+	Decode a position string to a tuple with the element sizes
+	Accepts either absolute positions or as a percentage of the full size.
+	Negative positions are subtracted from the size.
+	
+	'-50 50%' -> (750, 300) (given that size is (800,600))
+	"""
+	
+	if str == '':
+		return None
+	
+	# parse an element
+	# x is element
+	# s is the total dimension
+	def f(x,s):
+		if x[-1] == '%':
+			v = float(x[:-1]) / 100.0 * s
+		else:
+			v = float(x)
+		
+		if v < 0.0:
+			v = s + v
+		
+		return v
+	
+	# split into pieces 'x y' -> [x, y]
+	p = str.split(' ')
+	
+	# parse each element
+	p = [f(x,s) for x,s in zip(p, size)]
+	
+	# create tuple
+	return tuple(p)
+	
 class Template:
 	def __init__(self, filename):
-		doc = minidom.parse(filename)
-		print doc
+		self._filename = filename
 	
 	def rasterize(self, dst, size, params):
+		doc = minidom.parse(self._filename)
+		template = doc.getElementsByTagName('template')[0]
+		
 		w,h = size
 		data = array.array('c', chr(0) * w * h * 4)
 		surface = cairo.ImageSurface.create_for_data(data, cairo.FORMAT_ARGB32, w, h, w*4)
 		cr = cairo.Context(surface)
 		
 		cr.save()
-		cr.set_source_rgba(1,0,1,1)
+		cr.set_source_rgba(0,0,0,1)
 		cr.set_operator(cairo.OPERATOR_SOURCE)
 		cr.paint()
 		cr.restore()
 		
-		x=25.6
-		y=128.0
-		x1=102.4
-		y1=230.4
-		x2=153.6
-		y2=25.6
-		x3=230.4
-		y3=128.0
-		
-		cr.move_to (x, y);
-		cr.curve_to (x1, y1, x2, y2, x3, y3);
-		
-		cr.set_line_width (10.0);
-		cr.stroke ();
-		
-		cr.set_source_rgba (1, 0.2, 0.2, 0.6);
-		cr.set_line_width (6.0);
-		cr.move_to (x,y);   cr.line_to (x1,y1);
-		cr.move_to (x2,y2); cr.line_to (x3,y3);
-		cr.stroke ();
-		
+		for item in template.childNodes:
+			if not isinstance(item, minidom.Element):
+				continue
+			
+			cr.save()
+			try:
+				if item.tagName == 'text':
+					self._text(size, cr, item, params[item.getAttribute('name')])
+				elif item.tagName == 'textarea':
+					self._textarea(size, cr, item, params[item.getAttribute('name')])
+			finally:
+				cr.restore()
+			
 		surface.write_to_png(dst)
+	
+	@staticmethod
+	def _text(size, cr, item, text):
+		font = item.getAttribute('font') or 'Sans'
+		fontsize = float(item.getAttribute('size') or '36.0')
+		r,g,b,a = decode_color(item.getAttribute('color')) or (0,0,0,1)
+		x,y = decode_position(item.getAttribute('position'), size) or (0,0)
+		alignment = item.getAttribute('align')
+		
+		cr.select_font_face (font, cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_BOLD)
+		cr.set_font_size(fontsize)
+		cr.set_source_rgba(r,g,b,a)
+		
+		extents = cr.text_extents(text)
+		
+		offset = 0
+		if alignment == "center":
+			x -= extents[4] / 2
+		elif alignment == "right":
+			x -= extents[4]
+		
+		cr.move_to(x, y)
+		cr.show_text(text)
+	
+	@staticmethod
+	def _textarea(size, cr, item, text):
+		font = item.getAttribute('font') or 'Sans'
+		fontsize = float(item.getAttribute('size') or '36.0')
+		r,g,b,a = decode_color(item.getAttribute('color')) or (0,0,0,1)
+		x,y = decode_position(item.getAttribute('position'), size) or (0,0)
+		w,h = decode_position(item.getAttribute('boxsize'), size) or size
+		alignment = item.getAttribute('align')
+		
+		cr.set_source_rgba(r,g,b,a)
+		cr.move_to(x-w/2.0, y-h/2.0)
+		
+		ctx = pangocairo.CairoContext(cr)
+		layout = ctx.create_layout()
+		layout.set_font_description(pango.FontDescription('%s %d' % (font, fontsize)))
+		layout.set_width(w * pango.SCALE)
+		
+		if alignment == 'center':
+			layout.set_alignment(pango.ALIGN_CENTER)
+		elif alignment == 'right':
+			layout.set_alignment(pango.ALIGN_RIGHT)
+		elif alignment == 'justify':
+			layout.set_justify(True)
+		
+		layout.set_text(text);
+		ctx.show_layout(layout)
 
 class TextAssembler(Assembler):
 	def is_editable(self):
