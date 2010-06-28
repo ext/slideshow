49b3c17663dcc756429d2875ef6df2c313ffcd26
David Sveningsson <ext@sidvind.com>
2010-06-28 15:27:30 +0000

    putting resolution class in a separate file
    svn path=/branches/slideshow-python/; revision=735

--- lib/assembler/text.py
+++ lib/assembler/text.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from . import Assembler
+from lib.resolution import Resolution
 from settings import Settings
 import array, cairo, pango, pangocairo, json, re
 import xml
@@ -45,48 +46,6 @@ def decode_position(str, size):
 
 color_parser = ColorParser(components=4)
 decode_color = color_parser.parse
-
-def validate(func):
-    def decorate(input, max):
-        ret = func(input, max)
-        
-        try:
-            assert ret.w <= max.w
-            assert ret.h <= max.h
-            assert abs(input.aspect() - ret.aspect()) < 0.01
-        except:
-            print 'input: ', input
-            print 'size:  ', max
-            print 'return:', ret
-            raise
-        
-        return ret
-    return decorate
-
-class Resolution:
-	def __init__(self, w,h):
-		self.w = float(w)
-		self.h = float(h)
-	
-	def aspect(self):
-		return self.w / self.h
-	
-	def __iter__(self):
-		return (self.w, self.h).__iter__()
-	
-	@validate
-	def fit(self, max):
-		new_size = Resolution(max.w, max.h)
-		
-		if self.aspect() > max.aspect():
-			new_size.h = max.w * (self.h / self.w)
-		else:
-			new_size.w = max.h * (self.w / self.h)
-		
-		return new_size
-	
-	def __str__(self):
-		return '%dx%d %.4f' % (self.w, self.h, self.aspect())
 
 class Template:
 	def __init__(self, filename):
---
+++ lib/resolution.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def validate(func):
+    def decorate(input, max):
+        ret = func(input, max)
+        
+        try:
+            assert ret.w <= max.w
+            assert ret.h <= max.h
+            assert abs(input.aspect() - ret.aspect()) < 0.01
+        except:
+            print 'input: ', input
+            print 'size:  ', max
+            print 'return:', ret
+            raise
+        
+        return ret
+    return decorate
+
+class Resolution:
+    def __init__(self, w,h):
+        self.w = float(w)
+        self.h = float(h)
+    
+    def aspect(self):
+        return self.w / self.h
+    
+    def __iter__(self):
+        return (self.w, self.h).__iter__()
+    
+    @validate
+    def fit(self, max):
+        new_size = Resolution(max.w, max.h)
+        
+        if self.aspect() > max.aspect():
+            new_size.h = max.w * (self.h / self.w)
+        else:
+            new_size.w = max.h * (self.w / self.h)
+        
+        return new_size
+    
+    def __str__(self):
+        return '%dx%d %.4f' % (self.w, self.h, self.aspect())
