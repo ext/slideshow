93e4e0f9106c62a771a3b9b1eaad144eb87eb2f1
David Sveningsson <ext@sidvind.com>
2010-10-05 19:37:52 +0000

    video state stub
    svn path=/branches/slideshow-python/; revision=816

---
+++ src/state/VideoState.cpp
@@ -0,0 +1,39 @@
+/**
+ * This file is part of Slideshow.
+ * Copyright (C) 2008-2010 David Sveningsson <ext@sidvind.com>
+ *
+ * Slideshow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Slideshow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Slideshow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#	include "config.h"
+#endif
+
+#include "VideoState.h"
+#include "SwitchState.h"
+
+VideoState::VideoState(State* state, const char* filename)
+	: State(state)
+	, _filename(strdup(filename)){
+
+}
+
+VideoState::~VideoState(){
+	free(_filename);
+	_filename = NULL;
+}
+
+State* VideoState::action(bool &flip){
+	return new SwitchState(this);
+}
