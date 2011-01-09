4b1ad44c82a6b7a276f3d4f8f204dacabc6a4c00
David Sveningsson <ext@sidvind.com>
2011-01-09 23:26:20 +0000

    preparing to tag new release
    svn path=/trunk/; revision=911

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "slideshow",
-    version = '0.3.3', # remember to bump in 'slideshow/templates/index.html' as well
+    version = '0.3.3', # remember to bump 'configure.ac' and 'slideshow/templates/index.html' as well
     license = 'AGPL',
     author = 'David Sveningsson',
     author_email = 'ext@sidvind.com',
