9769611d7871fd8bb554ad7f13842e1f11c29ea7
David Sveningsson <ext@sidvind.com>
2011-01-29 15:13:39 +0000

    just using a single frame instead (the limitations of gif is quite high)
    svn path=/trunk/; revision=988

--- slideshow/lib/assembler/video.py
+++ slideshow/lib/assembler/video.py
@@ -18,39 +18,21 @@ class VideoAssembler(Assembler):
 		settings = Settings()
 		base = settings['Path.BasePath']
 		video = settings['Path.Video']
-		temp = settings['Path.Temp']
 		
-		cache = os.path.join(base, temp, src + '.gif')
-		# convert to gif (cached since this is kept the same size for all rasters)
-		if not os.path.exists(cache):
-			retcode = subprocess.call([
-				'ffmpeg',
-				'-i', os.path.join(base, video, filename),
-				'-pix_fmt', 'rgb24', # force rgb24
-				'-loop_output', '0', # loop forever
-				'-r', '1/25', # 1/25 fps
-				'-ss', '5', # just 5s into video
-				'-f', 'gif', # format
-				'-an', # skip audio
-				'-y', # overwrite dst
-				cache,
-			])
-			if retcode != 0:
-				raise ValueError, 'failed to resample %s' % (slide.raster_path(size))
-		
-		# asjust speed of gif
-		retcode = subprocess.call([
-			"convert",
-			'-delay', '100',
-			cache,
-			'-resize', str(size),
-			'-background', 'black',
-			'-gravity', 'center',
-			'-extent', str(size),
+		args = [
+			'ffmpeg',
+			'-i', os.path.join(base, video, filename),
+			'-ss', '5', # just 5s into video
+			'-s', str(size),
+			'-an', # skip audio
+			'-f', 'image2',
+			'-vframes', '1',
 			slide.raster_path(size)
-		])
-		if retcode != 0:
-			raise ValueError, 'failed to resample %s' % (slide.raster_path(size))
+		]
+		try:
+			retcode = subprocess.call(args)
+		except OSError, e:
+			raise RuntimeError, 'Failed to run `%s`: %s' % (' '.join([pipes.quote(x) for x in args]), e) 
 		
 		# try to create a symlink to the actual file, expected to fail since any
 		# previous rasterisations would had created this symlink already.
@@ -66,7 +48,7 @@ class VideoAssembler(Assembler):
 		return 'Video'
 	
 	def raster_extension(self):
-		return '.gif'
+		return '.png'
 	
 	def render(self, content, context):
 		content = content.copy()
