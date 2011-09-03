479046c572a08242ef9a2474253d3a9030125448
David Sveningsson <ext@sidvind.com>
2011-09-03 13:31:09 +0000

    fetching next slide
    svn path=/trunk/; revision=1033

--- slideshow/pages/instance.py
+++ slideshow/pages/instance.py
@@ -30,11 +30,58 @@ class Handler(object):
 			cherrypy.response.headers['Content-Type'] = 'text/plain'
 		
 		settings = Settings()
+		c = cherrypy.thread_data.db.cursor()
+		
+		row = self._get_next_slide(queue=settings['Runtime.queue'], context=id)
+        
+		if not row:
+			loop = c.execute("SELECT loop FROM queue WHERE id = :queue LIMIT 1", dict(queue=settings['Runtime.queue'])).fetchone()['loop']
+			row = {
+				'assembler': None,
+				'id': None,
+				'sortorder': None
+			}
+		
+			if loop:
+				tmp = self._get_next_slide(queue=settings['Runtime.queue'], context=-1)
+				row = tmp and tmp or row
 		
 		return json.dumps({
 			'version': 1,
-			'assembler': 'image',
+			'assembler': row['assembler'],
 			'filename': None,
-			'slide-id': 52,
-			'context': 0,
+			'slide-id': row['id'],
+			'context': row['sortorder'],
 		}, indent=4)
+
+	def _get_next_slide(self, **kwargs):
+		c = cherrypy.thread_data.db.cursor()
+		return c.execute("""
+		    SELECT
+		        id,
+		        path,
+		        sortorder,
+		        queue_id,
+		        assembler
+		    FROM
+		        slide
+		    WHERE
+		        queue_id = -1
+		UNION
+		    SELECT
+		        id,
+		        path,
+		        sortorder,
+		        queue_id,
+		        assembler
+		    FROM
+		        slide
+		    WHERE
+		        queue_id = :queue AND
+		        sortorder > :context
+		ORDER BY
+		    queue_id,
+		    sortorder
+		LIMIT 1;
+		    """, kwargs).fetchone()
+	
