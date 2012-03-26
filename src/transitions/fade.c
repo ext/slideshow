22722056abd70fcb5d9091372c9ac510b3e4adde
David Sveningsson <ext@sidvind.com>
2012-03-26 12:20:11 +0000

    using a buffer object on gpu
    svn path=/trunk/; revision=1064

--- src/transitions/fade.c
+++ src/transitions/fade.c
@@ -39,21 +39,28 @@ static unsigned char indices[4] = {
 	0, 1, 2, 3
 };
 
+static GLuint vbo[2];
+
 static void render(transition_context_t* context){
 	glPushClientAttrib(GL_VERTEX_ARRAY|GL_TEXTURE_COORD_ARRAY);
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-	glVertexPointer(2, GL_FLOAT, sizeof(vertex_t), &vertices[0].x);
-	glTexCoordPointer(2, GL_FLOAT, sizeof(vertex_t), &vertices[0].u);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexPointer(2,   GL_FLOAT, sizeof(vertex_t), (void*)0);
+	glTexCoordPointer(2, GL_FLOAT, sizeof(vertex_t), (void*)(sizeof(float)*2));
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[1]);
 
 	glColor4f(1,1,1,1);
 	glBindTexture(GL_TEXTURE_2D, context->texture[1]);
-	glDrawElements(GL_QUADS, 4, GL_UNSIGNED_BYTE, indices);
+	glDrawElements(GL_QUADS, 4, GL_UNSIGNED_BYTE, 0);
 
 	glColor4f(1,1,1,context->state);
 	glBindTexture(GL_TEXTURE_2D, context->texture[0]);
-	glDrawElements(GL_QUADS, 4, GL_UNSIGNED_BYTE, indices);
+	glDrawElements(GL_QUADS, 4, GL_UNSIGNED_BYTE, 0);
 
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glPopClientAttrib();
 }
 
@@ -63,6 +70,16 @@ void* module_alloc(){
 
 int EXPORT module_init(transition_module_t* module){
 	module->render = render;
+
+	/* generate VBO with vertices and indices */
+	glGenBuffers(2, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_t)*4, vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[1]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned char)*4, indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
 	return 0;
 }
 
