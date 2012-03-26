e1c7f94a4153acc444cfbcb55835b66880388a3b
David Sveningsson <ext@sidvind.com>
2012-03-26 00:35:08 +0000

    change indentation
    svn path=/trunk/; revision=1061

--- slideshow/pages/maintenance.py
+++ slideshow/pages/maintenance.py
@@ -8,117 +8,116 @@ import slideshow.daemon as daemon
 import slideshow.event as event
 
 class Ajax(object):
-	@cherrypy.expose
-	def log(self):
-		return '<p>' + '<br/>\n'.join(daemon.log()) + '</p>'
+    @cherrypy.expose
+    def log(self):
+        return '<p>' + '<br/>\n'.join(daemon.log()) + '</p>'
 
 class Handler(object):
-	ajax = Ajax()
-	
-	@cherrypy.expose
-	@template.output('maintenance/index.html', parent='maintenance')
-	def index(self):
-		settings = Settings()
-		cmd, args, env, cwd = daemon.settings(browser.from_settings(settings))
-		return template.render(log=daemon.log(), state=daemon.state(), cmd=cmd, args=args, env=env, cwd=cwd)
+    ajax = Ajax()
+    
+    @cherrypy.expose
+    @template.output('maintenance/index.html', parent='maintenance')
+    def index(self):
+        settings = Settings()
+        cmd, args, env, cwd = daemon.settings(browser.from_settings(settings))
+        return template.render(log=daemon.log(), state=daemon.state(), cmd=cmd, args=args, env=env, cwd=cwd)
 
-	@cherrypy.expose
-	@template.output('maintenance/config.html', parent='maintenance')
-	def config(self, action=None, **kwargs):
-		settings = Settings()
-		
-		if action == 'save':
-			error = False
-			with settings:
-				# hack for environmental variables
-				env = {}
-				if 'Env' in kwargs:
-					try:
-						env = kwargs['Env'].split("\r\n")
-						env = filter(lambda x: len(x) > 0, env)
-						env = map(lambda x: tuple(x.split('=')), env)
-						env = dict(env)
-					except:
-						raise ValueError, 'Malformed environment variables'
-					finally:
-						kwargs['Env'] = env
-				
-				for k,v in kwargs.items():
-					try:
-						settings[k] = v
-					except ValueError as e:
-						settings.item(k).message = str(e)
-						error = True
-			
-			if not error:
-				settings.persist()
-				raise cherrypy.HTTPRedirect('/maintenance')
-		
-		return template.render(settings=settings)
-	
-	@cherrypy.expose()
-	def reset(self):
-		daemon.reset()
-		raise cherrypy.HTTPRedirect('/maintenance')
-	
-	@cherrypy.expose
-	def start(self, resolution=None, fullscreen=True):
-		daemon.start(resolution, fullscreen)
-		raise cherrypy.HTTPRedirect('/maintenance')
-	
-	@cherrypy.expose
-	def stop(self):
-		daemon.stop()
-		raise cherrypy.HTTPRedirect('/maintenance')
-	
-	@cherrypy.expose
-	def reload(self):
-		daemon.reload()
-		raise cherrypy.HTTPRedirect('/maintenance')
-	
-	@cherrypy.expose
-	def coredump(selfs):
-		cherrypy.response.headers['Content-Type'] = 'application/octet-stream'
-		cherrypy.response.headers['Content-Disposition'] = 'attachment; filename=core'
-		
-		settings = Settings()
-		cwd = settings['Path.BasePath']
-		
-		return open(os.path.join(cwd, 'core'))
-	
-	@cherrypy.expose
-	def rebuild(self):
-		import sqlite3, threading
-		class Progress(threading.Thread):
-			def __init__(self):
-				threading.Thread.__init__(self)
-				self._sem = threading.Semaphore(value=0)
-				self._content = []
-			
-			def push(self, x):
-				self._content.append(x)
-				self._sem.release()
-			
-			def pop(self):
-				while True:
-					self._sem.acquire()
-					value = self._content.pop()
-					if value == None:
-						break
-					else:
-						yield str(value)
-			
-			def run(self):
-				cherrypy.thread_data.db = sqlite3.connect('site.db')
-				cherrypy.thread_data.db.row_factory = sqlite3.Row
-				cherrypy.thread_data.db.cursor().execute('PRAGMA foreign_keys = ON')
-				
-				event.trigger('maintenance.rebuild', self.push)
-				self.push(None)
-		
-		progress = Progress()
-		progress.start()
-		
-		return progress.pop()
-	rebuild._cp_config = {'response.stream': True}
-
+    @cherrypy.expose
+    @template.output('maintenance/config.html', parent='maintenance')
+    def config(self, action=None, **kwargs):
+        settings = Settings()
+        
+        if action == 'save':
+            error = False
+            with settings:
+                # hack for environmental variables
+                env = {}
+                if 'Env' in kwargs:
+                    try:
+                        env = kwargs['Env'].split("\r\n")
+                        env = filter(lambda x: len(x) > 0, env)
+                        env = map(lambda x: tuple(x.split('=')), env)
+                        env = dict(env)
+                    except:
+                        raise ValueError, 'Malformed environment variables'
+                    finally:
+                        kwargs['Env'] = env
+                
+                for k,v in kwargs.items():
+                    try:
+                        settings[k] = v
+                    except ValueError as e:
+                        settings.item(k).message = str(e)
+                        error = True
+            
+            if not error:
+                settings.persist()
+                raise cherrypy.HTTPRedirect('/maintenance')
+        
+        return template.render(settings=settings)
+    
+    @cherrypy.expose()
+    def reset(self):
+        daemon.reset()
+        raise cherrypy.HTTPRedirect('/maintenance')
+    
+    @cherrypy.expose
+    def start(self, resolution=None, fullscreen=True):
+        daemon.start(resolution, fullscreen)
+        raise cherrypy.HTTPRedirect('/maintenance')
+    
+    @cherrypy.expose
+    def stop(self):
+        daemon.stop()
+        raise cherrypy.HTTPRedirect('/maintenance')
+    
+    @cherrypy.expose
+    def reload(self):
+        daemon.reload()
+        raise cherrypy.HTTPRedirect('/maintenance')
+    
+    @cherrypy.expose
+    def coredump(selfs):
+        cherrypy.response.headers['Content-Type'] = 'application/octet-stream'
+        cherrypy.response.headers['Content-Disposition'] = 'attachment; filename=core'
+        
+        settings = Settings()
+        cwd = settings['Path.BasePath']
+        
+        return open(os.path.join(cwd, 'core'))
+    
+    @cherrypy.expose
+    def rebuild(self):
+        import sqlite3, threading
+        class Progress(threading.Thread):
+            def __init__(self):
+                threading.Thread.__init__(self)
+                self._sem = threading.Semaphore(value=0)
+                self._content = []
+            
+            def push(self, x):
+                self._content.append(x)
+                self._sem.release()
+            
+            def pop(self):
+                while True:
+                    self._sem.acquire()
+                    value = self._content.pop()
+                    if value == None:
+                        break
+                    else:
+                        yield str(value)
+            
+            def run(self):
+                cherrypy.thread_data.db = sqlite3.connect('site.db')
+                cherrypy.thread_data.db.row_factory = sqlite3.Row
+                cherrypy.thread_data.db.cursor().execute('PRAGMA foreign_keys = ON')
+                
+                event.trigger('maintenance.rebuild', self.push)
+                self.push(None)
+        
+        progress = Progress()
+        progress.start()
+        
+        return progress.pop()
+    rebuild._cp_config = {'response.stream': True}
