f808cdaa4e3746acac5a090f5abc3067e4f776c1
David Sveningsson <ext@sidvind.com>
2012-04-22 16:38:28 +0200

    Creating "modules" in javascript code.

--- slideshow/static/js/slideshow.js
+++ slideshow/static/js/slideshow.js
@@ -1,52 +1,82 @@
 /* global states */
 var delete_id = undefined; /* which slide is about to be deleted */
 
-$(document).ready(function(){
-		/* queue is a global variable */
-		for ( i in queue ){
-				var id = queue[i];
-				var other = queue.filter(function(x){ return x != id; });
-				$(id).bind('updated', function() {
-						var self = '#' + $(this).attr('id');
-						var list = $(this).sortable('toArray');
-						var n = list.length;
-
-						/* show/hide warning about empty queue */
-						if ( self == active ){
-								console.log('n: ' + n);
-								if ( notice_visible && n > 0 ){
-										console.log('hiding');
-										notice_visible = false;
-										$("#empty_notice").fadeOut("slow");
-								}
-								if ( !notice_visible && n == 0 ){
-										console.log('showing');
-										notice_visible = true;
-										$("#empty_notice").fadeIn("slow");
-								}
+queue = function(){
+		return {
+				sorting: function(){
+						/* test if queues exists on this page */
+						if ( typeof(queues) == 'undefined' ){
+								return;
 						}
 
-						/* notify server about update */
-						$.ajax({
-								type: "POST",
-								url: "/slides/ajax/move",
-								data: "queue=" + $(this).attr('id') + "&slides=" + list,
-								error: function(x, status, error){
-										alert(status + '\n' + error);
-								},
+						/* queues is a global variable */
+						for ( i in queues ){
+								var id = queues[i];
+								var other = queues.filter(function(x){ return x != id; });
+								$(id).bind('updated', function() {
+										var self = '#' + $(this).attr('id');
+										var list = $(this).sortable('toArray');
+										var n = list.length;
+										
+										/* show/hide warning about empty queue */
+										if ( self == active ){
+												if ( notice_visible && n > 0 ){
+														notice_visible = false;
+														$("#empty_notice").fadeOut("slow");
+												}
+												if ( !notice_visible && n == 0 ){
+														notice_visible = true;
+														$("#empty_notice").fadeIn("slow");
+												}
+										}
+										
+										/* notify server about update */
+										$.ajax({
+												type: "POST",
+												url: "/slides/ajax/move",
+												data: "queue=" + $(this).attr('id') + "&slides=" + list,
+												error: function(x, status, error){
+														alert(status + '\n' + error);
+												},
+										});
+								});
+								$(id).sortable({
+										connectWith: other,
+										placeholder: 'slide_placeholder',
+										tolerance: 'pointer',
+										distance: 10,
+										update: function(){
+												$(this).trigger('updated');
+										},
+								});
+								$(id).disableSelection();
+						}
+				},
+		};
+}();
+
+var slide = function(){
+		return {
+				delete: function(id){
+						delete_id = id;
+						$("#delete_dialog img").attr('src', '/slides/show/' + id + '/800/600');
+						$("#delete_dialog").dialog({
+								modal: true,
+								resizable: false,
+								position: 'center',
+								width: 834, /* 800 + 17 + 17 (padding) */
+								height: 700,
+								close: function(){
+										delete_id = undefined;
+								}
 						});
-				});
-				$(id).sortable({
-						connectWith: other,
-						placeholder: 'slide_placeholder',
-						tolerance: 'pointer',
-						distance: 10,
-						update: function(){
-								$(this).trigger('updated');
-						},
-				});
-				$(id).disableSelection();
+				},
 		}
+}();
+
+$(document).ready(function(){
+		/* enable sorting on main page */
+		queue.sorting();
 
 		/* setup delete dialog buttons */
 		$('#delete_cancel').bind('click', function(){
@@ -74,21 +104,4 @@ $(document).ready(function(){
 
 				});
 		});
-
 });
-
-function slide_delete(id){
-		delete_id = id;
-		$("#delete_dialog img").attr('src', '/slides/show/' + id + '/800/600');
-		$("#delete_dialog").dialog({
-				modal: true,
-				resizable: false,
-				position: 'center',
-				width: 834, /* 800 + 17 + 17 (padding) */
-				height: 700,
-				close: function(){
-						console.log('dialog closed');
-						delete_id = undefined;
-				}
-		});
-}
