5c04957ca44fc43eae034fe389d39b879ec000ae
David Sveningsson <ext@sidvind.com>
2012-04-24 00:39:31 +0200

    Add logging and some minor error handling.

--- slideshow/event.py
+++ slideshow/event.py
@@ -2,25 +2,27 @@
 # -*- coding: utf-8 -*-
 
 import types
+import cherrypy
 
 _subscribers = {}
 
 class func:
-    def __init__(self, inst, method):
+    def __init__(self, inst, method, cls):
         self._inst = inst
         self._method = method
-    
+        self._cls = cls
+
     def __call__(self, *args, **kwargs):
         # bind method to class instance
         types.MethodType(self._method, self._inst)(*args, **kwargs)
 
 def listener(cls):
-   for name, method in cls.__dict__.iteritems():
+    for name, method in cls.__dict__.iteritems():
         if hasattr(method, "subscribe"):
             # methods are still not bound, so store both the instance and the
             # method for late binding
-            subscribe(method.subscribe, func(cls, method))
-   return cls
+            subscribe(method.subscribe, func(cls, method, None))
+    return cls
 
 def callback(event):
     def decorate(func):
@@ -30,14 +32,19 @@ def callback(event):
     return decorate
 
 def trigger(event, *args, **kwargs):
+    cherrypy.engine.log('%s triggered' % event)
+
     if event not in _subscribers:
         return
-    
+
     for func in _subscribers[event]:
         func(*args, **kwargs)
 
 def subscribe(event, callback):
+    if not isinstance(event, basestring):
+        raise ValueError, 'event name must be string (got %s)' % event.__class__.__name__
+
     if event not in _subscribers:
         _subscribers[event] = []
-    
+
     _subscribers[event].append(callback)
