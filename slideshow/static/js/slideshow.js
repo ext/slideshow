a72baf3fa4eaabfadce0b09664978b02fcacdc67
David Sveningsson <ext@sidvind.com>
2012-04-24 21:51:49 +0200

    Moving to global javascript file.

--- slideshow/static/js/slideshow.js
+++ slideshow/static/js/slideshow.js
@@ -1,3 +1,7 @@
+String.prototype.repeat = function(num){
+		return new Array( num + 1 ).join( this );
+}
+
 /* global states */
 var delete_id = undefined; /* which slide is about to be deleted */
 
