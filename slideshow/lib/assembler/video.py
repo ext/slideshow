e5e87af0f237b42ba4828d4b00f13d795846b9a7
David Sveningsson <ext@sidvind.com>
2012-04-28 18:37:18 +0200

    Implement locadata for video assembler.

--- slideshow/lib/assembler/video.py
+++ slideshow/lib/assembler/video.py
@@ -8,23 +8,23 @@ import os, subprocess
 class VideoAssembler(Assembler):
 	def is_viewable(self):
 		return False
-	
+
 	def is_editable(self):
 		return False
-	
+
 	def is_playable(self):
 		return True
-	
+
 	def assemble(self, slide, filename, **kwargs):
 		return {'filename': filename}
-	
+
 	def rasterize(self, slide, size, params):
 		filename = params['filename']
 		src, ext = os.path.splitext(filename)
 		settings = Settings()
 		base = settings['Path.BasePath']
 		video = settings['Path.Video']
-		
+
 		args = [
 			'ffmpeg',
 			'-i', os.path.join(base, video, filename),
@@ -39,32 +39,27 @@ class VideoAssembler(Assembler):
 		try:
 			retcode = subprocess.call(args)
 		except OSError, e:
-			raise RuntimeError, 'Failed to run `%s`: %s' % (' '.join([pipes.quote(x) for x in args]), e) 
-		
+			raise RuntimeError, 'Failed to run `%s`: %s' % (' '.join([pipes.quote(x) for x in args]), e)
+
 		# try to create a symlink to the actual file, expected to fail since any
 		# previous rasterisations would had created this symlink already.
 		try:
 			os.symlink(os.path.join(base, video, filename), slide.src_path('video'))
 		except OSError:
 			pass
-	
+
 	def default_size(self, slide, params, width=None):
 		pass
-	
+
 	def title(self):
 		return 'Video'
-	
+
 	def raster_extension(self):
 		return '.png'
-	
-	def render(self, content, context):
-		content = content.copy()
-		
+
+	def localdata(self, content):
 		settings = Settings()
 		base = settings['Path.BasePath']
 		video = settings['Path.Video']
 		videopath = os.path.join(base, video)
-		
-		content['files'] = os.listdir(videopath)
-		
-		return Assembler.render(self, content, context)
+		return {'files': os.listdir(videopath)}
