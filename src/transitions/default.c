8b4e050c384ecac4d7ecfd29b443f1a8d45aac50
David Sveningsson <ext@sidvind.com>
2014-01-16 03:31:22 +0100

    transitions: provide a default implementation
    The common case is to perform all work in the fragment shader.

---
+++ src/transitions/default.c
@@ -0,0 +1,50 @@
+/**
+ * This file is part of Slideshow.
+ * Copyright (C) 2008-2013 David Sveningsson <ext@sidvind.com>
+ *
+ * Slideshow is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Slideshow is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Slideshow.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This is a default implementation for transition plugins.
+ *
+ * It setups a shader (assumes datapack entries "vertex_shader" and
+ * "fragment_shader" exists) and loads the uniforms required. Default fsquad
+ * rendering is used.
+ */
+
+#include "graphics.h"
+#include "Transition.h"
+#include "gl.h"
+
+void* EXPORT module_alloc(){
+	return malloc(sizeof(transition_module_t));
+}
+
+int EXPORT module_init(transition_module_t* module){
+	module->render = NULL; /* use default renderer using fsquad */
+
+	/* create shader */
+	module->shader = graphics_load_shader(
+		SHADER_VERTEX,   &vertex_shader,
+		SHADER_FRAGMENT, &fragment_shader,
+		SHADER_NONE
+	);
+	if ( !module->shader ) return 1;
+
+	/* setup uniforms */
+	module->state_uniform = glGetUniformLocation(module->shader, "s");
+
+	return 0;
+}
--- src/transitions/vfade.c
+++ src/transitions/vfade.c
@@ -1,6 +1,6 @@
 /**
  * This file is part of Slideshow.
- * Copyright (C) 2008 David Sveningsson <ext@sidvind.com>
+ * Copyright (C) 2008-2013 David Sveningsson <ext@sidvind.com>
  *
  * Slideshow is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -17,30 +17,7 @@
  */
 
 #include "module.h"
-#include "graphics.h"
-#include "Transition.h"
-#include "gl.h"
 #include "vfade_files.h"
+#include "default.c"
 
 MODULE_INFO("Vertical fade", TRANSITION_MODULE, "David Sveningsson");
-
-void* module_alloc(){
-	return malloc(sizeof(transition_module_t));
-}
-
-int EXPORT module_init(transition_module_t* module){
-	module->render = NULL; /* use default renderer using fsquad */
-
-	/* create shader */
-	module->shader = graphics_load_shader(
-		SHADER_VERTEX, &vertex_shader,
-		SHADER_FRAGMENT, &fragment_shader,
-		SHADER_NONE
-	);
-	if ( !module->shader ) return 1;
-
-	/* setup uniforms */
-	module->state_uniform = glGetUniformLocation(module->shader, "s");
-
-	return 0;
-}
