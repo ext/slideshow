eafd425596743fa73edcd013e56bdd6f99b95d9b
David Sveningsson <ext@sidvind.com>
2014-04-07 22:26:31 +0200

    arguments

--- slideshow/theme.py
+++ slideshow/theme.py
@@ -6,8 +6,11 @@ from slideshow.lib.assembler.text import TextAssembler, Theme
 from slideshow.lib.resolution import Resolution
 
 # arguments
-size = (800,600)
-filename = 'nitroxy13.xml'
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--size', default=(800,600), type=lambda x: x.split('x'))
+parser.add_argument('src', metavar='THEME')
+parser.add_argument('dst', metavar='OUTPUT')
+args = parser.parse_args()
 
 # load stub settings
 root = dirname(slideshow.__file__)
@@ -17,14 +20,14 @@ settings.load(join(root, 'settings.xml'), config_file=None, format_keys=dict(bas
 # load assembler
 asm = TextAssembler()
 params = {
-    'resolution': size,
+    'resolution': args.size,
 }
 
 # load theme
-theme = Theme(filename)
+theme = Theme(args.src)
 for item in theme.items():
     if item.name in params: continue
     params[item.name] = item.name
 
-with open('test.png','w') as fp:
-    asm.rasterize(Resolution(*size), params, file=fp, theme=filename)
+with open(args.dst,'w') as fp:
+    asm.rasterize(Resolution(*args.size), params, file=fp, theme=args.src)
