90a2f382b8ce74b2dfd5f0aab1ae1f4c6a8e471a
David Sveningsson <ext@sidvind.com>
2014-04-12 23:13:40 +0200

    cosmetic: remove trailing whitespace

--- slideshow/pages/queue.py
+++ slideshow/pages/queue.py
@@ -10,14 +10,14 @@ class Ajax(object):
         id = int(id[6:]) # remove prefix
         c = cherrypy.thread_data.db
         q = queue.from_id(c, id)
-        
+
         if q is None:
             return ''
-        
+
         q.rename(c, value)
         cherrypy.thread_data.db.commit()
         return value
-    
+
     @cherrypy.expose
     def remove(self, id):
         id = int(id)
@@ -28,19 +28,19 @@ class Ajax(object):
         c = cherrypy.thread_data.db
         if not queue.delete(c, id):
             return 'false'
-        
+
         cherrypy.thread_data.db.commit()
         return 'true'
 
 class Handler(object):
     ajax = Ajax()
-    
+
     @cherrypy.expose
     @template.output('queue/index.html', parent='queues')
     def index(self):
         queues = queue.all(cherrypy.thread_data.db)
         return template.render(queues=queues)
-    
+
     @cherrypy.expose
     def add(self, name, submit):
         name = name.decode('utf-8')
@@ -48,25 +48,25 @@ class Handler(object):
         queue.add(c, name)
         cherrypy.thread_data.db.commit()
         raise cherrypy.HTTPRedirect('/queue')
-    
+
     @cherrypy.expose
     @template.output('queue/rename.html', parent='queues')
     def rename(self, id, name=None, submit=None):
         c = cherrypy.thread_data.db
         q = queue.from_id(c, id)
-        
+
         if name is not None:
             q.rename(c, name)
             cherrypy.thread_data.db.commit()
             raise cherrypy.HTTPRedirect('/queue')
-        
+
         return template.render(queue=q)
-    
+
     @cherrypy.expose
     def activate(self, id):
         queue.activate(id)
         raise cherrypy.HTTPRedirect('/')
-    
+
     @cherrypy.expose
     def loop(self, id, state):
         queue.set_loop(id, state)
