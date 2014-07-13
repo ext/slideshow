48a66fb1621f3b2acf53358867ec4589ea6788db
David Sveningsson <ext@sidvind.com>
2014-07-13 22:22:57 +0200

    frontend: verify that executables exists in path
    references #114

--- slideshow/web/__init__.py
+++ slideshow/web/__init__.py
@@ -27,6 +27,22 @@ import slideshow.tools.ipblock
 def get_resource_path(*path):
     return os.path.join(os.path.dirname(slideshow.__file__), *path)
 
+def daemon_exists():
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    for program in ['slideshow-daemon', 'slideshow-transition']:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                break
+        else:
+            print >> sys.stderr, program, 'could not be found in PATH.'
+            return False
+
+    return True
+
 class Command:
     def __init__(self, *args):
         self.cmd = args
@@ -204,6 +220,12 @@ def run():
         if not os.access(args.config_file, os.W_OK):
             print >> sys.stderr, 'Could not write to configuration file:', args.config_file
             print >> sys.stderr, 'Make sure the user has proper permissions.'
+            sys.exit(1)
+
+        # verify slideshow daemon exists in path
+        if not daemon_exists():
+            # Notice about specific application is written by `daemon_exists`.
+            print >> sys.stderr, 'Either install to a location in PATH or adjust PATH to include the directory where it is installed.'
             sys.exit(1)
 
         # update basepath and references if using --switch
